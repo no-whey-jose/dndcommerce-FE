91efb60c37af47ca58c4602164ba486b7424b599
Jose-Antonio Rubio <jadrubio@gmail.com>
2022-06-14 14:56:13 -0400

    implemented a navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './routes/nav-bar/nav-bar';
 import Home from './routes/home/home';
+
+const Shop = () => {
+  return <div>Shop Placeholder</div>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/nav-bar/nav-bar.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/DnD_Location.svg';
+import './nav-bar.styles.scss';
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="nav-bar">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default NavBar;
