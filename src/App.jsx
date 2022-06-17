f53410b73d7192dee53b0e93bdf699df849faa97
Jose-Antonio Rubio <jadrubio@gmail.com>
2022-06-17 13:10:57 -0400

    created authentication page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './routes/nav-bar/nav-bar';
 import Home from './routes/home/home';
-import SignIn from './routes/sign-in/sign-in';
+import Authentication from './routes/authentication/authentication';
 
 const Shop = () => {
   return <div>Shop Placeholder</div>
@@ -13,7 +13,7 @@ const App = () => {
       <Route path="/" element={<NavBar />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
